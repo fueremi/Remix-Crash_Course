b16e5e2b002ff6267acac31f20f3d596d2fa4695
Rian Oktio Mersa Putra <rianoktiomersaputra4@gmail.com>
2021-12-21 11:44:17 +0700

    finish crash course remix

---
+++ app/root.jsx
@@ -0,0 +1,72 @@
+import { Outlet, LiveReload, Link, Links, Meta } from "remix";
+import globalStylesUrl from "~/styles/global.css";
+
+export default function App() {
+  return (
+    <Document>
+      <Layout>
+        <Outlet />
+      </Layout>
+    </Document>
+  );
+}
+
+export const links = () => [{ rel: "stylesheet", href: globalStylesUrl }];
+
+export const meta = () => {
+  const description = "A cool blog build with Remix";
+  const keywords = "remix, react, javascript";
+  return {
+    description,
+    keywords,
+  };
+};
+
+function Document({ children, title }) {
+  return (
+    <html lang="en">
+      <head>
+        <meta charSet="utf-8" />
+        <meta name="viewport" content="width=device-width" />
+        <Meta />
+        <Links />
+        <title>{title ? title : "Remix Blog"}</title>
+      </head>
+      <body>
+        {children}
+        {process.env.NODE_ENV === "development" ? <LiveReload /> : null}
+      </body>
+    </html>
+  );
+}
+
+function Layout({ children }) {
+  return (
+    <>
+      <nav className="navbar">
+        <Link to="/" className="logo">
+          Remix
+        </Link>
+        <ul className="nav">
+          <li>
+            <Link to="/posts/">Posts</Link>
+          </li>
+        </ul>
+      </nav>
+
+      <div className="container">{children}</div>
+    </>
+  );
+}
+
+export function ErrorBoundary({ error }) {
+  console.log(error);
+  return (
+    <Document>
+      <Layout>
+        <h1>Error</h1>
+        <p>{error.message}</p>
+      </Layout>
+    </Document>
+  );
+}
