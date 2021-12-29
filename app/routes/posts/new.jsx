9aea6beab924b571e1d23a81b1969527f81e9af4
Rian Oktio Mersa Putra <rianoktiomersaputra4@gmail.com>
2021-12-29 20:48:17 +0700

    add validation on a new post

--- app/routes/posts/new.jsx
+++ app/routes/posts/new.jsx
@@ -1,5 +1,21 @@
-import { Link, redirect } from "remix";
+import { Link, redirect, useActionData, json } from "remix";
 import { db } from "~/utils/db.server";
+
+function validateTitle(title) {
+  if (typeof title !== "string" || title.length <= 3) {
+    return "Title must be at least 3 characters";
+  }
+}
+
+function validateBody(body) {
+  if (typeof body !== "string" || body.length <= 6) {
+    return "Body must be at least 6 characters";
+  }
+}
+
+function badRequest(data) {
+  return json(data, { status: 400 });
+}
 
 export const action = async ({ request }) => {
   const form = await request.formData();
@@ -7,12 +23,24 @@ export const action = async ({ request }) => {
   const body = form.get("body");
   const fields = { title, body };
 
+  const fieldErrors = {
+    title: validateTitle(title),
+    body: validateBody(body),
+  };
+
+  if (Object.values(fieldErrors).some(Boolean)) {
+    console.log(fieldErrors);
+    return badRequest({ fieldErrors, fields });
+  }
+
   const post = await db.post.create({ data: fields });
 
   return redirect(`/posts/${post.id}`);
 };
 
 function NewPost() {
+  const actionData = useActionData();
+
   return (
     <>
       <div className="page-header">
@@ -27,10 +55,21 @@ function NewPost() {
           <div className="form-control">
             <label htmlFor="title">Title</label>
             <input type="text" name="title" id="title" />
+            <div className="error">
+              <p>
+                {actionData?.fieldErrors?.title &&
+                  actionData?.fieldErrors?.title}
+              </p>
+            </div>
           </div>
           <div className="form-control">
             <label htmlFor="body">Body</label>
             <textarea type="text" name="body" id="body" />
+            <div className="error">
+              <p>
+                {actionData?.fieldErrors?.body && actionData?.fieldErrors?.body}
+              </p>
+            </div>
           </div>
 
           <button type="submit" className="btn btn-block">
